8ee8fa5d8411b0e612d92b421dc9f38ec4b7d391
shaik abbas <shaikabbas051@gmail.com>
2017-08-16 10:43:42 +0530

    added display and score

--- js/app.js
+++ js/app.js
@@ -13,25 +13,25 @@
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+    Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
-        this.x = this.x + this.speed*dt;
+            this.x = this.x + this.speed*dt;
     // which will ensure the game runs at the same speed for
     // all computers.
-//check for collision. If collided return to starting point
-    if (((this.y > player.y) && (this.y < player.y+30)) && ((this.x > player.x - 60) && (this.x < player.x + 50))){
-        player.x = 200;
-        player.y = 380;
-    }
-    if(this.x >= 505){
-        this.x = 0;
-    }
-};
+    //check for collision. If collided return to starting point
+        if (((this.y > player.y) && (this.y < player.y+30)) && ((this.x > player.x - 60) && (this.x < player.x + 50))){
+            player.x = 200;
+            player.y = 380;
+        }
+        if(this.x >= 505){
+            this.x = 0;
+        }
+    };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    Enemy.prototype.render = function() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
 
 // Now write your own player class
     var Player = function(x, y, speed){
@@ -41,26 +41,28 @@ Enemy.prototype.render = function() {
         this.sprite = 'images/char-cat-girl.png';
     };
 // This class requires an update(), render() and
- Player.prototype.update = function(dt){
-    if (this.y <= 0){
-        player.x = 200;
-        player.y = 380;     // return to the initial position when reached the final row
-        score+=1;           //increase the score and level by 1
-        level+=1;
-    }
-    if (this.y > 375){
-        this.y = 375;
-    }
-    if (this.x < 0){
-        this.x = 0;
-    }
-    if (this.x > 400){
-        this.x = 400;
-    }
+     Player.prototype.update = function(dt){
+        if (this.y <= 0){
+            player.x = 200;
+            player.y = 380;     // return to the initial position when reached the final row
+            difficult(score);
+            score+=1;           //increase the score and level by 1
+            level+=1;
+        }
+        if (this.y > 375){
+            this.y = 375;
+        }
+        if (this.x < 0){
+            this.x = 0;
+        }
+        if (this.x > 400){
+            this.x = 400;
+        }
 
-};
+    };
     Player.prototype.render = function(){
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        scoreLevel(score, level);
     };
 // a handleInput() method.
     Player.prototype.handleInput = function(keyPress){
@@ -78,23 +80,30 @@ Enemy.prototype.render = function() {
         }
     };
 //display score and level
-var scoreLevel = function(score, level){
-    $(".runs").append("Score:" + score +"/" + "level:" + level)
-}
+    var scoreLevel = function(s, l){
+        var canvas = document.getElementsByTagName('canvas');
+        var firstTag = canvas[0];
+        scoreLevelDiv.innerHTML = 'Score:' + s + '/'+ 'Level:' + l; 
+        document.body.insertBefore(scoreLevelDiv, firstTag[0]);
+    }
 
+    var difficult = function(moreEnemies){
+
+    }
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-    var allEnemies = [new Enemy(0, 60, 100), new Enemy(0, 145, 50), new Enemy(0, 230, 50)];
+    
 
 // Place the player object in a variable called player
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 //input
+    var allEnemies = [new Enemy(0, 60, 100), new Enemy(0, 145, 50), new Enemy(0, 230, 50)];
     var player = new Player(200, 380, 50);
     var score=0;
     var level=1;
-
-document.addEventListener('keyup', function(e) {
+    var scoreLevelDiv = document.createElement('div');
+    document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -103,4 +112,4 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
-});
+    });
